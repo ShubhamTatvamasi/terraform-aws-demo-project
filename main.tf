3f3578acb8a24246c609d2ee59cd69a507d57d7a
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-03-14 09:25:49 +0530

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
+variable "my_ip" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -32,6 +33,7 @@ resource "aws_internet_gateway" "myapp-igw" {
 
 resource "aws_default_route_table" "main-rtb" {
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.myapp-igw.id
@@ -41,8 +43,43 @@ resource "aws_default_route_table" "main-rtb" {
   }
 }
 
+# resource "aws_security_group" "myapp-sg" {
+resource "aws_default_security_group" "default-sg" {
+  # name = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    # "Name" = "${var.env_prefix}-sg"
+    "Name" = "${var.env_prefix}-default-sg"
+  }
+}
+
+
 # resource "aws_route_table" "myapp-route-table" {
 #   vpc_id = aws_vpc.myapp-vpc.id
+# 
 #   route {
 #     cidr_block = "0.0.0.0/0"
 #     gateway_id = aws_internet_gateway.myapp-igw.id
