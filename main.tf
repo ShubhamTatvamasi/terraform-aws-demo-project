d7ca778f265e2172c1bf8689e6c31805753b2ae3
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2021-03-14 14:51:14 +0530

    modified:   main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "instance_type" {}
+variable "public_key_location" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -89,14 +91,47 @@ data "aws_ami" "latest-amazon-linux-image" {
   }
 }
 
-output "aws_ami" {
-  value = data.aws_ami.latest-amazon-linux-image
-}
-
-# resource "aws_instance" "myapp-server" {
-#   ami = data.aws_ami.latest-amazon-linux-image.id
+# output "aws_ami_id" {
+#   value = data.aws_ami.latest-amazon-linux-image.id
 # }
 
+resource "aws_key_pair" "ssh-key" {
+  key_name = "server-key"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [ aws_default_security_group.default-sg.id ]
+  availability_zone = var.avail_zone
+
+  associate_public_ip_address = true
+
+  # key_name = "server-key-pair"
+  key_name = aws_key_pair.ssh-key.key_name
+
+  user_data = file("entry-script.sh")
+
+  # user_data = <<EOF
+  #   #!/bin/bash
+  #   sudo yum update -y
+  #   sudo yum install -y docker
+  #   sudo systemctl start docker
+  #   sudo usermod -aG docker ec2-user
+  #   docker run -d -p 8080:80 nginx
+  # EOF
+
+  tags = {
+    "Name" = "${var.env_prefix}-server"
+  }
+}
+
+output "myapp-server-public-ip" {
+  value = aws_instance.myapp-server.public_ip
+}
 
 # resource "aws_route_table" "myapp-route-table" {
 #   vpc_id = aws_vpc.myapp-vpc.id
